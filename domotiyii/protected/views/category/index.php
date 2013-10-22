7ceac95d039b30ce9e06973cb1af64c1cfeb4d2e
Jesse Kerkhoven <jesse.kerkhoven@gmail.com>
2013-10-22 12:01:28 +0000

    Fixed: unversioned code

---
+++ domotiyii/protected/views/category/index.php
@@ -0,0 +1,77 @@
+<?php
+/* @var $this CategoryController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->widget('bootstrap.widgets.TbBreadcrumb', array(
+    'links' => array(
+        Yii::t('translate','Categories'),
+    ),
+));
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+    $('.search-form').slideToggle('fast');
+    return false;
+});
+$('.search-form form').submit(function(){
+    $.fn.yiiGridView.update('all-categories-grid', {
+        data: $(this).serialize()
+    });
+    return false;
+});
+");
+
+$this->beginWidget('zii.widgets.CPortlet', array(
+        'htmlOptions'=>array(
+                'class'=>''
+        )
+));
+$this->widget('bootstrap.widgets.TbNav', array(
+        'type'=>TbHtml::NAV_TYPE_PILLS,
+        'items'=>array(
+                array('label'=>Yii::t('translate','List'), 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'),'active'=>true, 'linkOptions'=>array()),
+                array('label'=>Yii::t('translate','Search'), 'icon'=>'icon-search', 'url'=>'#', 'linkOptions'=>array('class'=>'search-button')),
+                array('label'=>Yii::t('translate','Create'), 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'), 'linkOptions'=>array()),
+        ),
+));
+$this->endWidget();
+?>
+
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+        'model'=>$model,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('domotiyii.LiveGridView', array(
+    'id'=>'all-categories-grid',
+    'refreshTime'=>Yii::app()->params['refreshCategories'], // x second refresh as defined in config
+    'type'=>'striped condensed',
+    'dataProvider'=>$model->search(),
+    'template'=>'{items}{pager}{summary}',
+    'selectableRows' => 1,
+    'columns'=>array(
+        array('name'=>'id', 'header'=>'#', 'htmlOptions'=>array('width'=>'20')),
+        array('name'=>'name', 'header'=>Yii::t('translate','Name'), 'htmlOptions'=>array('width'=>'150')),
+        array('class'=>'bootstrap.widgets.TbButtonColumn',
+           'template'=> Yii::app()->user->isGuest ? '{view}' : '{view} {update} {delete}',
+           'header'=>Yii::t('translate','Actions'),
+           'htmlOptions'=>array('style'=>'width: 40px'),
+           'buttons'=>array(
+              'view' => array(
+                 'label'=>Yii::t('translate','View'),
+                 'url'=>'Yii::app()->controller->createUrl("view", array("id"=>$data["id"]))',
+              ),
+              'update' => array(
+                 'label'=>Yii::t('translate','Edit'),
+                 'url'=>'Yii::app()->controller->createUrl("update", array("id"=>$data["id"]))',
+              ),
+              'delete' => array(
+                 'label'=>Yii::t('translate','Delete'),
+                 'url'=>'Yii::app()->controller->createUrl("delete", array("id"=>$data["id"],"command"=>"delete"))',
+              ),
+           ),
+        ),
+    ),
+));
+?>
