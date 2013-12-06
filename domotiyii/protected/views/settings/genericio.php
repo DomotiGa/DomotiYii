30cdb5e71bd6d5dc4d7558d07c55d7cb0fa60a70
cyberjunky <support@domotiga.nl>
2013-12-06 10:56:53 +0000

    Added missing modules for lighttpd install. Fixed GenericIO boolean view values.

--- domotiyii/protected/views/settings/genericio.php
+++ domotiyii/protected/views/settings/genericio.php
@@ -17,14 +17,14 @@ $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 
 <legend>Generic I/O</legend>
 <fieldset>
-		<?php echo $form->checkBoxControlGroup($model,'enabled'); ?>
+		<?php echo $form->checkBoxControlGroup($model,'enabled', array('value'=>-1)); ?>
                 <?php echo $form->dropDownListControlGroup($model,'type', array('serial' => 'serial', 'tcp' => 'tcp'), array('onchange'=>'switchType(this);')); ?>
                 <?php echo $form->textFieldControlGroup($model,'tcphost', array('readonly'=>($model->type == 'serial')? true : false, 'id'=>'tcphost')); ?>
                 <?php echo $form->numberFieldControlGroup($model,'tcpport', array('readonly'=>($model->type == 'serial')? true : false, 'id'=>'tcpport')); ?> <?php echo $form->textFieldControlGroup($model,'serialport', array('class'=>'span5', 'readonly'=>($model->type == 'serial')? false : true, 'id'=>'serialport')); ?>
                 <?php echo $form->dropDownListControlGroup($model,'baudrate', array('9600' => '9600', '19200' => '19200', '38400' => '38400', '57600' => '57600', '115200' => '115200'), array('readonly'=>($model->type == 'serial')? false : true, 'id'=>'baudrate')); ?>
 		<?php echo $form->textFieldControlGroup($model,'regex'); ?>
 		<?php echo $form->textFieldControlGroup($model,'delimiter'); ?>
-		<?php echo $form->checkBoxControlGroup($model,'debug'); ?>
+		<?php echo $form->checkBoxControlGroup($model,'debug', array('value'=>-1)); ?>
 </fieldset>
 
 <?php echo TbHtml::formActions(array(
