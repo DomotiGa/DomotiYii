cb2d43486eb335bd591ef9f976dce30c5cf6dcb1
Patrice Vialard <p65174@gmail.com>
2014-02-16 14:17:07 +0100

    essais de fenetre modal pour resoudre problem des events

--- domotiyii/protected/controllers/ActionsController.php
+++ domotiyii/protected/controllers/ActionsController.php
@@ -1,125 +1,124 @@
 <?php
 
-class ActionsController extends Controller
-{
-	/**
- 	* Lists all actions.
-	*/
-	public function actionIndex()
-	{
-		$criteria = new CDbCriteria();            
+class ActionsController extends Controller {
 
-		$model=new Actions('search');
-		$model->unsetAttributes();  // clear any default values
+    /**
+     * Lists all actions.
+     */
+    public function actionIndex() {
+        $criteria = new CDbCriteria();
 
-		if(isset($_GET['Actions']))
-		{
-			$model->attributes=$_GET['Actions'];
+        $model = new Actions('search');
+        $model->unsetAttributes();  // clear any default values
 
-			if (!empty($model->id)) $criteria->addCondition('id = "'.$model->id.'"');
-			if (!empty($model->name)) $criteria->addCondition('name = "'.$model->name.'"');
-			if (!empty($model->description)) $criteria->addCondition('description = "'.$model->description.'"');
-			if (!empty($model->type)) $criteria->addCondition('type = "'.$model->type.'"');
-		}
-		$this->render('index', array('model'=>$model));
-	}
+        if (isset($_GET['Actions'])) {
+            $model->attributes = $_GET['Actions'];
 
-        public function actionView($id)
-        {
-                $model = Actions::model()->findByPk($id);
-                $this->render('view', array('model'=>$model));
+            if (!empty($model->id))
+                $criteria->addCondition('id = "' . $model->id . '"');
+            if (!empty($model->name))
+                $criteria->addCondition('name = "' . $model->name . '"');
+            if (!empty($model->description))
+                $criteria->addCondition('description = "' . $model->description . '"');
+            if (!empty($model->type))
+                $criteria->addCondition('type = "' . $model->type . '"');
         }
+        $this->render('index', array('model' => $model));
+    }
 
-        public function actionUpdate($id)
-        {
-                $model = Actions::model()->findByPk($id);
-                if(isset($_POST['Actions']))
-                {
-                        $model->attributes=$_POST['Actions'];
-                        if($model->validate())
-                        {
-                                // form inputs are valid, do something here
-                                $this->do_save($model);
-                        }
-                }
-                $this->render('update',array(
-                        'model'=>$model,
-                ));
+    public function actionView($id) {
+        $model = Actions::model()->findByPk($id);
+        if (isset($_GET['AJAXMODAL'])) {
+            echo $this->renderPartial('_form', array('model' => $model, 'readonly' => 1), TRUE, true);
+        } else {
+            $this->render('view', array('model' => $model));
         }
+    }
 
-        public function actionDelete($id)
-        {
-                // delete the entry from the "actions" table
-                $model = Actions::model()->findByPk($id);
-                $this->do_delete($model);
-	}
-
-        public function actionCreate()
-        {
-                $model=new Actions;
-
-                // Uncomment the following line if AJAX validation is needed
-                // $this->performAjaxValidation($model);
-
-                if(isset($_POST['Actions']))
-                {
-                        $model->attributes=$_POST['Actions'];
-                        if($model->validate())
-                        {
-                                $this->do_save($model);
-                        }
-                }
-                $this->render('create',array(
-                        'model'=>$model,
-                ));
+    public function actionUpdate($id) {
+        $model = Actions::model()->findByPk($id);
+        if (isset($_POST['Actions'])) {
+            $model->attributes = $_POST['Actions'];
+            if ($model->validate()) {
+                // form inputs are valid, do something here
+                $this->do_save($model);
+            }
         }
-
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
-
-        protected function do_save($model)
-        {
-                if ($model->save() === false)
-                {
-                        Yii::app()->user->setFlash('error', Yii::t('app','Action save failed!'));
-                } else {
-                        Yii::app()->user->setFlash('success', Yii::t('app','Action saved.'));
-                }
+        if (isset($_GET['AJAXMODAL'])) {
+            echo $this->renderPartial('_form', array('model' => $model), TRUE, true);
+        } else {
+            $this->render('update', array(
+                'model' => $model,
+            ));
         }
+    }
 
-        protected function do_delete($model)
-        {
+    public function actionDelete($id) {
+        // delete the entry from the "actions" table
+        $model = Actions::model()->findByPk($id);
+        $this->do_delete($model);
+    }
 
-                if ($model->delete() === false)
-                {
-                        Yii::app()->user->setFlash('error', Yii::t('app','Action delete failed!'));
-                } else {
-                        Yii::app()->user->setFlash('success', Yii::t('app','Action deleted.'));
-                        $this->redirect(array('index'));
-                }
+    public function actionCreate() {
+        $model = new Actions;
+
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
+
+        if (isset($_POST['Actions'])) {
+            $model->attributes = $_POST['Actions'];
+            if ($model->validate()) {
+                $this->do_save($model);
+            }
         }
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
+
+    // Uncomment the following methods and override them if needed
+    /*
+      public function filters()
+      {
+      // return the filter configuration for this controller, e.g.:
+      return array(
+      'inlineFilterName',
+      array(
+      'class'=>'path.to.FilterClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+
+      public function actions()
+      {
+      // return external action classes, e.g.:
+      return array(
+      'action1'=>'path.to.ActionClass',
+      'action2'=>array(
+      'class'=>'path.to.AnotherActionClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+     */
+
+    protected function do_save($model) {
+        if ($model->save() === false) {
+            Yii::app()->user->setFlash('error', Yii::t('app', 'Action save failed!'));
+        } else {
+            Yii::app()->user->setFlash('success', Yii::t('app', 'Action saved.'));
+        }
+    }
+
+    protected function do_delete($model) {
+
+        if ($model->delete() === false) {
+            Yii::app()->user->setFlash('error', Yii::t('app', 'Action delete failed!'));
+        } else {
+            Yii::app()->user->setFlash('success', Yii::t('app', 'Action deleted.'));
+            $this->redirect(array('index'));
+        }
+    }
+
 }
--- domotiyii/protected/views/events/view/_actions.php
+++ domotiyii/protected/views/events/view/_actions.php
@@ -3,38 +3,63 @@
 /* @var $model Devices */
 
 $config = array();
-$dataProvider = new CArrayDataProvider($rawData=$model->actions, $config);
+$dataProvider = new CArrayDataProvider($rawData = $model->actions, $config);
 
 $this->widget('domotiyii.LiveGridView', array(
-    'id'=>'all-devices-grid',
-    'type'=>'striped condensed',
-    'dataProvider'=>$dataProvider,
-    'template'=>'{items}{pager}{summary}',
-    'columns'=>array(
-        array('name'=>'id', 'header'=>'#', 'htmlOptions'=>array('width'=>'20')),
-        array('name'=>'name', 'header'=>Yii::t('app','Name'), 'htmlOptions'=>array('width'=>'120')),
-        array('name'=>'description', 'header'=>Yii::t('app','Description'), 'htmlOptions'=>array('width'=>'120')),
-        array('class'=>'bootstrap.widgets.TbButtonColumn',
-           'template'=> Yii::app()->user->isGuest ? '{view}' : '{view} {update} {delete}',
-           'header'=>Yii::t('app','Actions'),
-           'htmlOptions'=>array('style'=>'width: 40px'),
-           'buttons'=>array(
-              'view' => array(
-                 'label'=>Yii::t('app','View'),
-                 'url'=>'Yii::app()->controller->createUrl("actions/view", array("id"=>$data["id"]))',
-              ),
-              'update' => array(
-                 'label'=>Yii::t('app','Edit'),
-                 'url'=>'Yii::app()->controller->createUrl("actions/update", array("id"=>$data["id"]))',
-              ),
-              'delete' => array(
-                 'label'=>Yii::t('app','Delete'),
-                 'url'=>'Yii::app()->controller->createUrl("actions/delete", array("id"=>$data["id"],"command"=>"delete"))',
-              ),
-           ),
+    'id' => 'all-devices-grid',
+    'type' => 'striped condensed',
+    'dataProvider' => $dataProvider,
+    'template' => '{items}{pager}{summary}',
+    'columns' => array(
+        array('name' => 'id', 'header' => '#', 'htmlOptions' => array('width' => '20')),
+        array('name' => 'name', 'header' => Yii::t('app', 'Name'), 'htmlOptions' => array('width' => '120')),
+        array('name' => 'description', 'header' => Yii::t('app', 'Description'), 'htmlOptions' => array('width' => '120')),
+        array('class' => 'bootstrap.widgets.TbButtonColumn',
+            'template' => Yii::app()->user->isGuest ? '{view}' : '{view} {update} {delete}',
+            'header' => Yii::t('app', 'Actions'),
+            'htmlOptions' => array('style' => 'width: 40px'),
+            'buttons' => array(
+                'view' => array(
+                    'label' => Yii::t('app', 'View'),
+                    'url' => '',
+                ),
+                'update' => array(
+                    'label' => Yii::t('app', 'Edit'),
+                    'url' => 'Yii::app()->controller->createUrl("actions/update", array("id"=>$data["id"]))',
+                ),
+                'delete' => array(
+                    'label' => Yii::t('app', 'Delete'),
+                    'url' => 'Yii::app()->controller->createUrl("actions/delete", array("id"=>$data["id"],"command"=>"delete"))',
+                ),
+            ),
         ),
     ),
+));
+?>
+<?php $this->widget('bootstrap.widgets.TbModal', array(
+    'id' => 'action',
+    'header' => 'Actions Form',
+    'content' => '',
+    'footer' => array(
+        TbHtml::button('Save Changes', array('data-dismiss' => 'modal', 'color' => TbHtml::BUTTON_COLOR_PRIMARY)),
+        TbHtml::button('Close', array('data-dismiss' => 'modal')),
+     ),
 )); ?>
-
-
-
+ 
+<?php echo TbHtml::button('Click me to open modal', array(
+    'style' => TbHtml::BUTTON_COLOR_PRIMARY,
+    'size' => TbHtml::BUTTON_SIZE_LARGE,
+    'data-toggle' => 'modal',
+    'data-target' => '#action',
+)); ?>
+<button type="button" id="updateAction" data-target="#action" data-toggle="modal">Action</button>
+<script>
+    $('.view').on('click',function(){
+        $('#updateAction').click();
+        $('#action').find('.modal-body').text('coucou');
+    });
+    
+    function view(id) {
+        $('#action').find('.modal-body').text('coucou');
+    }
+    </script>
