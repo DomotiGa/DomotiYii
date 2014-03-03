2cf0635f38031abcc0d929790fca7f159a734b56
Ron Klinkien <ron@cyberjunky.nl>
2014-03-03 13:12:02 +0100

    Added: Missing Device Values form files.

---
+++ domotiyii/protected/views/devicevalues/view.php
@@ -0,0 +1,53 @@
+<?php
+/* @var $this DeviceValuesController */
+/* @var $model DeviceValues */
+
+$this->widget('bootstrap.widgets.TbBreadcrumb', array(
+    'links' => array(
+        Yii::t('app','DeviceValues') => 'index',  
+        Yii::t('app','View'),
+    ),
+));
+
+$this->beginWidget('zii.widgets.CPortlet', array(
+        'htmlOptions'=>array(
+                'class'=>''
+        )
+));
+$this->widget('bootstrap.widgets.TbNav', array(
+        'type'=>TbHtml::NAV_TYPE_PILLS,
+        'items'=>array(
+                array('label'=>Yii::t('app','List'), 'icon'=>'icon-th-list', 'url'=>Yii::app()->controller->createUrl('index'), 'linkOptions'=>array()),
+                array('label'=>Yii::t('app','Create'), 'icon'=>'icon-plus', 'url'=>Yii::app()->controller->createUrl('create'), 'linkOptions'=>array()),
+                array('label'=>Yii::t('app','View'), 'icon'=>'icon-eye-open', 'url'=>array('view', 'id'=>$model->id), 'active'=>true, 'linkOptions'=>array()),
+                array('label'=>Yii::t('app','Edit'), 'icon'=>'icon-edit', 'url'=>array('update', 'id'=>$model->id)),
+                array('label'=>Yii::t('app','Delete'), 'icon'=>'icon-trash', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>Yii::t('app','Are you sure you want to delete this value?')))
+        ),
+));
+$this->endWidget();
+?>
+
+<legend><?php echo $model->valuenum;?></legend>
+
+<?php $this->widget('domotiyii.DetailView', array(
+	'type' => 'striped condensed',
+	'data'=>$model,
+        'attributes'=>array(
+                array('name' => 'device_id'),
+                array('name' => 'valuenum'),
+                array('name' => 'value'),
+                array('name' => 'correction'),
+                array('name' => 'units'),
+                array('name' => 'log'),
+                array('name' => 'logdisplay'),
+                array('name' => 'logspeak'),
+                array('name' => 'rrd'),
+                array('name' => 'graph'),
+                array('name' => 'valuerrdsname'),
+                array('name' => 'valuerrdtype'),
+                array('name' => 'lastchanged'),
+                array('name' => 'lastseen'),
+                array('name' => 'type'),
+                array('name' => 'description'),
+	),
+)); ?>
