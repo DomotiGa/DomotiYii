c272bf6f6197aad3b740544b8f98d172d32f6bb7
Ron Klinkien <ron@cyberjunky.nl>
2014-03-04 15:20:14 +0100

    Fixed: Some devicevalue fields fixed.

--- domotiyii/protected/views/devicevalues/_form.php
+++ domotiyii/protected/views/devicevalues/_form.php
@@ -14,11 +14,11 @@
 
         <?php echo $form->textFieldControlGroup($model,'device_id'); ?>
         <?php echo $form->textFieldControlGroup($model,'valuenum'); ?>
-        <?php echo $form->textFieldControlGroup($model,'units'); ?>
-        <?php echo $form->textFieldControlGroup($model,'valuerrddsname'); ?>
-        <?php echo $form->textFieldControlGroup($model,'valuerrdtype'); ?>
         <?php echo $form->textFieldControlGroup($model,'value'); ?>
-        <?php echo $form->textFieldControlGroup($model,'correction'); ?>
+        <?php echo $form->dropDownListControlGroup($model,'units', array('°' => '°', '°C' => '°C', '°F' => '°F', '%' => '%', '€' => '€', '$' => '$', 'Amp' => 'Amp', 'Count' => 'Count', 'hPa' => 'hPa', 'Volt' => 'Volt', 'kWh' => 'kWh', 'kg' => 'kg', 'W' => 'W', 'Wh' => 'Wh', 'Watt' => 'Watt', 'Level' => 'Level', 'lb' => 'lb', 'RSSI' => 'RSSI', 'm/s' => 'm/s', 'mbar' => 'mbar', 'mm' => 'mm', 'mm/hr' => 'mm/hr', 'm3' => 'm3'), array('prompt'=>'')); ?>
+        <?php echo $form->textFieldControlGroup($model,'valuerrddsname'); ?>
+	<?php echo $form->dropDownListControlGroup($model,'valuerrdtype', array('GAUGE' => 'GAUGE', 'COUNTER' => 'COUNTER', 'DERIVE' => 'DERIVE', 'ABSOLUTE' => 'ABSOLUTE'), array('prompt'=>'')); ?>
+	<?php echo $form->textFieldControlGroup($model,'correction',array('class'=>'span5')); ?>
 	<?php echo $form->checkBoxControlGroup($model,'log', array('value'=>-1)); ?>
 	<?php echo $form->checkBoxControlGroup($model,'logdisplay', array('value'=>-1)); ?>
 	<?php echo $form->checkBoxControlGroup($model,'logspeak', array('value'=>-1)); ?>
@@ -27,7 +27,7 @@
         <?php echo $form->textFieldControlGroup($model,'lastchanged'); ?>
         <?php echo $form->textFieldControlGroup($model,'lastseen'); ?>
         <?php echo $form->textFieldControlGroup($model,'type'); ?>
-        <?php echo $form->textFieldControlGroup($model,'description'); ?>
+	<?php echo $form->textFieldControlGroup($model,'description',array('class'=>'span5')); ?>
 
 </fieldset>
 
