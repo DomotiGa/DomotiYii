137cd06bf2f42752d68369da60cf4180003504b3
Jesse Kerkhoven <jesse.kerkhoven@gmail.com>
2015-06-06 11:23:52 +0200

    Remove hidden field, prevent server from errors

--- domotiyii/protected/views/devices/edit/_location.php
+++ domotiyii/protected/views/devices/edit/_location.php
@@ -1,3 +1,10 @@
+<?php
+    // Remove wrong string form instead array of checkbox list
+    $cs = Yii::app()->getClientScript();
+    $cs->registerScript("checkboxgroups", '$( document ).ready(function() { $("#ytDevices_groupsarray").remove(); });');
+
+?>
+
                 <?php //echo $form->textFieldControlGroup($model,'groups',array('size'=>60,'maxlength'=>128)); ?>
                 <?php echo $form->inlineCheckBoxListControlGroup($model,'groupsarray',CHtml::listData(Groups::model()->findAll(),'name','name'),array('label'=>Yii::t('app','Groups'),'size'=>60,'maxlength'=>128)); ?>
                 <?php echo $form->dropDownListControlGroup($model,'location_id', $model->getLocations(),array('id'=>'location_id')); ?>
